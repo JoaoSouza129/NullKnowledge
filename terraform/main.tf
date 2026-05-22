5bb4aeab2f2e84a6ca57a1a5f5879aabee97abf8
Miguel Duarte <mig.duarte96@gmail.com>
2026-05-22 12:11:15 +0100

    terraform e k3s

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+resource "null_resource" "k3s_setup" {
+  connection {
+    type        = "ssh"
+    host        = var.droplet_ip
+    user        = var.ssh_user
+    private_key = file(var.ssh_private_key_path)
+    timeout     = "5m"
+  }
+
+  provisioner "file" {
+    source      = "${path.module}/install-k3s.sh"
+    destination = "/tmp/install-k3s.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/install-k3s.sh",
+      "/tmp/install-k3s.sh"
+    ]
+  }
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,3 @@
+droplet_ip           = "159.89.20.141"
+ssh_private_key_path = "~/.ssh/id_rsa"
+ssh_user             = "root"
