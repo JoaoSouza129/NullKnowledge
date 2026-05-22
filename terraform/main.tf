af85a94b4a23487ca568142653c9f9d4dba62f45
Miguel Duarte <mig.duarte96@gmail.com>
2026-05-22 12:17:49 +0100

    corrigir main

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 resource "null_resource" "k3s_setup" {
-  connection {
-    type        = "ssh"
-    host        = var.droplet_ip
-    user        = var.ssh_user
-    private_key = file(pathexpand(var.ssh_private_key_path))
-    timeout     = "5m"
+   connection {
+     type        = "ssh"
+     host        = var.droplet_ip
+     user        = var.ssh_user
+     agent       = true
+     timeout     = "5m"
   }
 
   provisioner "file" {
